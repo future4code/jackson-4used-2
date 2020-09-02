2ac0c58fcc576da94af1ebf6b6f8c706c4b3ceae
Daniel Melo <danielfarias380@gmail.com>
2020-09-02 11:35:32 -0300

    criação do componente FormularioDeCriacao

---
+++ src/components/FormularioDeCriacao.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+  border: 1px solid black;
+`
+
+class FormularioDeCriacao extends Component {
+  render() {
+    return (
+      <Container>
+        FormularioDeCriacao
+      </Container>
+    )
+  }
+}
+
+export default FormularioDeCriacao
