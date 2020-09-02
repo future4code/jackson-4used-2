e02b7aa31777e489ce99278d93d603f94145a8d4
unknown <leo.oliveira.b@gmail.com>
2020-09-02 16:28:48 -0300

    footer-criado

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,15 +1,23 @@
 import React, { Component } from 'react'
 import styled from 'styled-components'
+import coracaoFooter from '../img/coracaoFooter.png'
+
 
 const Container = styled.footer`
   border: 1px solid black;
+  border-bottom: none;
+  margin-left: 10vw;
+  margin-right: 10vw;
+  text-align: center;
+  background-color:#43ADA5;
+  height:8vh;
 `
 
 class Footer extends Component {
   render() {
     return (
       <Container>
-        Footer
+        <p><strong>DESENVOLVIDO COM <img src={coracaoFooter} width="15px"></img> PELA EQUIPE DE MARAVILHOSIDADES</strong></p> 
       </Container>
     )
   }
