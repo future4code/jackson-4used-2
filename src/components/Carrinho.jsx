15fc2c1e665ed58fbc0a0e90a8c0806567ac4405
Daniel Melo <danielfarias380@gmail.com>
2020-09-02 11:36:26 -0300

    criação do componente Carrinho

---
+++ src/components/Carrinho.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+  border: 1px solid black;
+`
+
+class Carrinho extends Component {
+  render() {
+    return (
+      <Container>
+        Carrinho
+      </Container>
+    )
+  }
+}
+
+export default Carrinho
