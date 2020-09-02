fb54591fdad52626d92793f00490aa8fe9549e25
unknown <leo.oliveira.b@gmail.com>
2020-09-02 15:50:48 -0300

    icone-logo-e-icone-carrinho-adicionado

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 import styled from 'styled-components'
 import Button from '@material-ui/core/Button'
 import TextField from '@material-ui/core/TextField'
+import LogoSemLogoMarca from '../img/LogoSemLogoMarca.png'
+import LocalGroceryStoreIcon from '@material-ui/icons/LocalGroceryStore';
 
 const Container = styled.header`
   display:flex;
@@ -9,21 +11,21 @@ const Container = styled.header`
   margin-left: 10vw;
   margin-right: 10vw;
   padding: 15px;
-  
-  
-`
+ `
 
 class Header extends Component {
   render() {
     return (
       <Container>
-        ícone 
+        <img src={LogoSemLogoMarca} width='60px' height='40px' ></img>
         <TextField label="Buscar" size="small" variant="outlined" />
         <Button>inicio</Button>
         <Button>Moças</Button>
         <Button>Rapazes</Button>
         <Button>Kids</Button>
-        <Button>Carrinho</Button>
+        <Button>
+          <LocalGroceryStoreIcon color='primary'/>
+        </Button>
         <Button variant="contained" color="primary">
           Quero Vender
         </Button>
