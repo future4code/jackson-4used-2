840817ecc43454f0d968c7a58ade8146232b2e93
Daniel Melo <danielfarias380@gmail.com>
2020-09-02 16:52:47 -0300

    remove importação de FIltros e heading no Main

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,7 +3,6 @@ import styled from 'styled-components'
 
 import GridDeCategorias from './GridDeCategorias'
 import PaginaDeProdutos from './PaginaDeProdutos'
-import Filtros from './Filtros'
 import GridDeProdutos from './GridDeProdutos'
 import FormularioDeCriacao from './FormularioDeCriacao'
 import Carrinho from './Carrinho'
@@ -17,10 +16,8 @@ class Main extends Component {
   render() {
     return (
       <Container>
-        <h1>Main</h1>
         <GridDeCategorias></GridDeCategorias>
         <PaginaDeProdutos></PaginaDeProdutos>
-        <Filtros></Filtros>
         <GridDeProdutos></GridDeProdutos>
         <FormularioDeCriacao></FormularioDeCriacao>
         <Carrinho></Carrinho>
