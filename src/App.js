37b2ad498a7e8a892a132e7f87aca8b2cebcbc08
Daniel Melo <danielfarias380@gmail.com>
2020-09-02 11:31:54 -0300

    reestruturação do componente APP

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import React from 'react'
-import { AppContainer } from './components/AppContainer'
 
-function App() {
-	return (
-        <AppContainer />
-	)
+import PaginaInicial from './components/PaginaInicial'
+import Layout from './components/Layout'
+
+class App extends React.Component {
+	constructor(props) {
+		super(props)
+
+		this.state = {
+			pagina: 2
+		}
+	}
+
+	render() {
+		return (
+			<React.Fragment>
+				{this.state.pagina === 1 ? <PaginaInicial /> : <Layout />}
+			</React.Fragment>
+		)
+	}
+
+
 }
 
 export default App
