e4d817ea77823d42673a0da90bb02c31050ae2a8
Daniel Melo <danielfarias380@gmail.com>
2020-09-02 11:33:07 -0300

    criação do componente PaginaInicial

---
+++ src/components/PaginaInicial.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class PaginaInicial extends Component {
+  render() {
+    return (
+      <h1>Pagina Inicial</h1>
+    )
+  }
+}
+
+export default PaginaInicial
